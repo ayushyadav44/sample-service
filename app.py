45aca994dbf43563930a8de908d6f613ca8b0029
Ayush Yadav <ayush.yadav@credgenics.com>
2026-03-26 11:39:21 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/healthz")
+def health_check():
+    """Health check endpoint"""
+    return {"status": "ok"}
+
+
+@app.get("/main")
+def main():
+    """Main endpoint"""
+    return {"sample-service": "Service is live and working fine"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
