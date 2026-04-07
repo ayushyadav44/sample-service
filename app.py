b75a9ee3021f9c59ec47c99c5f9e46130a306ece
Ayush Yadav <ayush.yadav@credgenics.com>
2026-04-08 01:18:58 +0530

    image updater

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def health_check():
 @app.get("/argocd-poc/main")
 def main():
     """Main endpoint"""
-    return {"sample-service": "Service is live and working fine, alongwith image updater"}
+    return {"sample-service": "Service is live and working fine, alongwith image updater, test-2"}
 
 @app.get("/argocd-poc/config")
 def get_config():
